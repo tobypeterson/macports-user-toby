a393ee3c81cdbe14b72a4e3edb54a66987b1f8a9
Toby Peterson <toby@macports.org>
2009-07-23 03:33:44 +0000

    Merge the MPParser object into MPPort, there's no need for the distinction.
    git-svn-id: https://svn.macports.org/repository/macports/users/toby@54211 d073be05-634f-4543-b044-5fe20cf6d1d6

--- objcports/MPPort.h
+++ objcports/MPPort.h
@@ -3,37 +3,19 @@
 @interface MPPort : NSObject
 {
 	NSURL *_url;
-	MPParser *_parser;
 
 	NSMutableDictionary *_variableInfo;
 	NSMutableDictionary *_variables;
 
 	NSMutableArray *_platforms;
 	NSMutableDictionary *_variants;
+
+	Tcl_Interp *_interp;
 }
 
-- (id)initWithURL:(NSURL *)url options:(NSDictionary *)options;
 - (id)initWithPath:(NSString *)url options:(NSDictionary *)options;
-
-- (NSString *)portfile;
-
-- (NSArray *)targets;
-- (BOOL)isTarget:(NSString *)target;
-
-- (NSArray *)variables;
 - (NSString *)variable:(NSString *)name;
-
-- (NSArray *)settableVariables;
-- (void)variable:(NSString *)var set:(NSArray *)value;
-
-- (NSArray *)settableArrayVariables;
-- (void)variable:(NSString *)var append:(NSArray *)value;
-- (void)variable:(NSString *)var delete:(NSArray *)value;
-
-- (NSArray *)definedPlatforms;
-- (BOOL)testAndRecordPlatform:(NSArray *)platform;
-
 - (NSArray *)definedVariants;
-- (BOOL)testAndRecordVariant:(NSString *)variant withProperties:(NSDictionary *)props;
+- (NSArray *)definedPlatforms;
 
 @end
--- objcports/MPPort.m
+++ objcports/MPPort.m
@@ -3,10 +3,23 @@
 #include <sys/utsname.h>
 
 #include "MPPort.h"
-#include "MPParser.h"
+#include "MPArrayAdditions.h"
 
 static NSString *kPortVariableType = @"Type";
 static NSString *kPortVariableConstant = @"Constant";
+
+static void command_create(Tcl_Interp *interp, const char *cmdName, ClientData clientData);
+static char *variable_read(ClientData clientData, Tcl_Interp *interp, const char *name1, const char *name2, int flags);
+static int _nslog(ClientData clientData, Tcl_Interp *interp, int objc, Tcl_Obj *const objv[]);
+static int _fake_boolean(ClientData clientData, Tcl_Interp *interp, int objc, Tcl_Obj *const objv[]);
+
+@interface MPPort (private)
+- (NSString *)portfile;
+- (NSArray *)targets;
+- (NSArray *)variables;
+- (NSArray *)settableVariables;
+- (NSArray *)settableArrayVariables;
+@end
 
 @implementation MPPort
 
@@ -53,8 +66,81 @@ static NSString *kPortVariableConstant = @"Constant";
 		[_variableInfo setObject:[NSDictionary dictionaryWithObject:@"Array" forKey:kPortVariableType] forKey:[NSString stringWithFormat:@"%@.cmd", command]];
 	}
 
-	_parser = [[MPParser alloc] initWithPort:self];
-
+	_interp = Tcl_CreateInterp();
+	Tcl_MakeSafe(_interp);
+	Tcl_UnsetVar(_interp, "tcl_version", 0);
+	Tcl_UnsetVar(_interp, "tcl_patchLevel", 0);
+	Tcl_UnsetVar(_interp, "tcl_platform", 0);
+	Tcl_DeleteCommand(_interp, "tell");
+	Tcl_DeleteCommand(_interp, "eof");
+	// XXX: etc?
+	
+	@try {
+		Tcl_Preserve(_interp);
+		
+		Tcl_CreateObjCommand(_interp, "nslog", _nslog, NULL, NULL); // XXX: debugging
+		//Tcl_Eval(_interp, "nslog [info commands]");
+		
+		command_create(_interp, "PortSystem", self);
+		command_create(_interp, "PortGroup", self);
+		command_create(_interp, "platform", self);
+		command_create(_interp, "variant", self);
+		
+		for (NSString *target in [self targets]) {
+			command_create(_interp, [target UTF8String], self);
+			command_create(_interp, [[@"pre-" stringByAppendingString:target] UTF8String], self);
+			command_create(_interp, [[@"post-" stringByAppendingString:target] UTF8String], self);
+		}
+		
+		for (NSString *opt in [self settableVariables]) {
+			command_create(_interp, [opt UTF8String], self);
+		}
+		for (NSString *opt in [self settableArrayVariables]) {
+			command_create(_interp, [[opt stringByAppendingString:@"-append"] UTF8String], self);
+			command_create(_interp, [[opt stringByAppendingString:@"-delete"] UTF8String], self);
+		}
+		
+		for (NSString *var in [self variables]) {
+			Tcl_TraceVar(_interp, [var UTF8String], TCL_TRACE_READS, variable_read, self);
+		}
+		
+		// bogus targets
+		Tcl_CreateObjCommand(_interp, "pre-activate", _nslog, NULL, NULL); // XXX: debugging
+		Tcl_CreateObjCommand(_interp, "post-activate", _nslog, NULL, NULL); // XXX: debugging
+		Tcl_CreateObjCommand(_interp, "pre-install", _nslog, NULL, NULL); // XXX: debugging
+		Tcl_CreateObjCommand(_interp, "post-install", _nslog, NULL, NULL); // XXX: debugging
+		Tcl_CreateObjCommand(_interp, "post-pkg", _nslog, NULL, NULL); // XXX: debugging
+		Tcl_CreateObjCommand(_interp, "post-mpkg", _nslog, NULL, NULL); // XXX: debugging
+		Tcl_CreateObjCommand(_interp, "archive", _nslog, NULL, NULL); // XXX: debugging
+		Tcl_CreateObjCommand(_interp, "install", _nslog, NULL, NULL); // XXX: debugging
+		Tcl_CreateObjCommand(_interp, "activate", _nslog, NULL, NULL); // XXX: debugging
+		Tcl_CreateObjCommand(_interp, "unarchive", _nslog, NULL, NULL); // XXX: debugging
+		Tcl_CreateObjCommand(_interp, "post-clean", _nslog, NULL, NULL); // XXX: debugging
+		
+		// functions we need to provide (?)
+		Tcl_CreateObjCommand(_interp, "variant_isset", _fake_boolean, NULL, NULL); // XXX: debugging
+		Tcl_CreateObjCommand(_interp, "variant_set", _fake_boolean, NULL, NULL); // XXX: debugging
+		Tcl_CreateObjCommand(_interp, "tbool", _fake_boolean, NULL, NULL); // XXX: debugging
+		Tcl_CreateObjCommand(_interp, "strsed", _nslog, NULL, NULL); // XXX: debugging
+		Tcl_CreateObjCommand(_interp, "suffix", _nslog, NULL, NULL); // XXX: debugging
+		Tcl_CreateObjCommand(_interp, "include", _nslog, NULL, NULL); // XXX: debugging
+		
+		// variables that should be constant
+		Tcl_CreateObjCommand(_interp, "prefix", _nslog, NULL, NULL);
+		
+		if (Tcl_EvalFile(_interp, [[self portfile] UTF8String]) != TCL_OK) {
+			NSLog(@"Tcl_EvalFile(): %s", Tcl_GetStringResult(_interp));
+			exit(1);
+		}
+		
+		Tcl_Release(_interp);
+	}
+	@catch (NSException *exception) {
+		NSLog(@"%@: %@", [exception name], [exception reason]);
+		[self release];
+		self = nil;
+	}
+	
 	return self;
 }
 
@@ -70,7 +156,6 @@ static NSString *kPortVariableConstant = @"Constant";
 
 - (void)dealloc
 {
-	[_parser release];
 	[_url release];
 
 	[_variableInfo release];
@@ -78,6 +163,8 @@ static NSString *kPortVariableConstant = @"Constant";
 
 	[_platforms release];
 	[_variants release];
+
+	Tcl_DeleteInterp(_interp);
 
 	[super dealloc];
 }
@@ -263,4 +350,125 @@ static NSString *kPortVariableConstant = @"Constant";
 	return YES;
 }
 
+- (void)performCommand:(NSString *)command arguments:(NSArray *)args
+{
+	if ([command isEqualToString:@"PortSystem"]) {
+		assert([args count] == 1);
+		assert([[args objectAtIndex:0] isEqualToString:@"1.0"]);
+	} else if ([command isEqualToString:@"PortGroup"]) {
+		NSLog(@"ignoring %@, grps r hard m'kay", command);
+		// XXX: this should probably set some state in parent port instance
+		// (ugh, more tcl parsing)
+	} else if ([command isEqualToString:@"platform"]) {
+		NSUInteger count = [args count];
+		NSString *os = nil;
+		id release = [NSNull null];
+		id arch = [NSNull null];
+		
+		if (count < 2 || count > 4) {
+			NSLog(@"bogus platform declaration");
+			return;
+		}
+		
+		os = [args objectAtIndex:0];
+		
+		if (count == 3) {
+			NSInteger rel = [[args objectAtIndex:1] integerValue];
+			if (rel != 0) {
+				release = [NSNumber numberWithInteger:rel];
+			} else {
+				arch = [args objectAtIndex:1];
+			}
+		} else if (count == 4) {
+			release = [NSNumber numberWithInteger:[[args objectAtIndex:1] integerValue]];
+			arch = [args objectAtIndex:2];
+		}
+		
+		if ([self testAndRecordPlatform:[NSArray arrayWithObjects:os, release, arch, nil]]) {
+			Tcl_Eval(_interp, [[args lastObject] UTF8String]);
+		}
+	} else if ([command isEqualToString:@"variant"]) {
+		NSUInteger count = [args count];
+		NSString *name;
+		NSMutableDictionary *props;
+		int i;
+		
+		// variant name [a b c d] {}
+		if (count < 2 || count % 2) {
+			NSLog(@"bogus variant declaration");
+			return;
+		}
+		
+		name = [args objectAtIndex:0];
+		
+		// this isn't quite right, conflicts can take multiple "arguments"
+		props = [NSMutableDictionary dictionaryWithCapacity:count-2];
+ 		for (i = 1; i < count - 1; i += 2) {
+			[props setObject:[args objectAtIndex:i+1] forKey:[args objectAtIndex:i]];
+		}
+		
+		if ([self testAndRecordVariant:name withProperties:props]) {
+			Tcl_Eval(_interp, [[args lastObject] UTF8String]);
+		}
+	} else if ([self isTarget:command]) {
+		// XXX: store for later use...
+	} else {
+		if ([command hasSuffix:@"-append"]) {
+			[self variable:[command substringWithRange:NSMakeRange(0, [command length] - 7)] append:args];
+		} else if ([command hasSuffix:@"-delete"]) {
+			[self variable:[command substringWithRange:NSMakeRange(0, [command length] - 7)] delete:args];
+		} else {
+			[self variable:command set:args];
+		}
+	}
+}
+
 @end
+
+static int
+command_trampoline(ClientData clientData, Tcl_Interp *interp, int objc, Tcl_Obj *const objv[])
+{
+	NSArray *args = [[NSArray alloc] initWithTclObjects:objv count:objc];
+	[(MPPort *)clientData performCommand:[args objectAtIndex:0] arguments:[args subarrayWithRange:NSMakeRange(1, [args count] - 1)]];
+	[args release];
+	
+	return TCL_OK;
+}
+
+static void
+command_create(Tcl_Interp *interp, const char *cmdName, ClientData clientData)
+{
+	Tcl_CmdInfo info;
+	if (Tcl_GetCommandInfo(interp, cmdName, &info) != 0) {
+		NSLog(@"Command '%s' already exists, bailing.", cmdName);
+		abort();
+	}
+	Tcl_CreateObjCommand(interp, cmdName, command_trampoline, clientData, NULL);
+}
+
+static char *
+variable_read(ClientData clientData, Tcl_Interp *interp, const char *name1, const char *name2, int flags)
+{
+	NSString *var = [(MPPort *)clientData variable:[NSString stringWithUTF8String:name1]];
+	assert(var != nil);
+	Tcl_SetVar2Ex(interp, name1, name2, Tcl_SubstObj(interp, Tcl_NewStringObj([var UTF8String], -1), TCL_SUBST_VARIABLES), 0);
+	return NULL;
+}
+
+// debugging
+static int
+_nslog(ClientData clientData, Tcl_Interp *interp, int objc, Tcl_Obj *const objv[])
+{
+	NSArray *args = [[NSArray alloc] initWithTclObjects:++objv count:--objc];
+	NSLog(@"%@", [args componentsJoinedByString:@" "]);
+	[args release];
+	
+	return TCL_OK;
+}
+
+static int
+_fake_boolean(ClientData clientData, Tcl_Interp *interp, int objc, Tcl_Obj *const objv[])
+{
+	Tcl_SetObjResult(interp, Tcl_NewBooleanObj(0));
+	return TCL_OK;
+}
